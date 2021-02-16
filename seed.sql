2b756b26b8bdf19cdb20a266a1fe68c95574b8ce
jansferr <sferrazzajane@gmail.com>
2021-02-16 11:20:45 -0500

    seed corrected

--- seed.sql
+++ seed.sql
@@ -1,14 +1,14 @@
 Create database greatbaydb;
 
 drop database greatbaydb;
-
-create table bid {
-    id int Primary key auto_increment not null,
+use greatbaydb;
+create table bid (
+    id int not null Primary key auto_increment,
     item VARCHAR(30),
     price int 
-};
+);
 
-insert into (item, price) values
+insert into bid (item, price) values
 ("chairs", 2),
 ("tables", 2),
 ("lamps", 2);
