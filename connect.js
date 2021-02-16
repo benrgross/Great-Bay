f8f2fd650d9e3e375575c75474b985d538d9c808
jansferr <sferrazzajane@gmail.com>
2021-02-16 13:21:13 -0500

    post changed and working

--- connect.js
+++ connect.js
@@ -3,48 +3,120 @@ const inquirer = require("inquirer");
 const { type } = require("os");
 const { bindCallback } = require("rxjs");
 const { exit } = require("process");
+const util = require('util');
+
 
 const connection = mysql.createConnection({
   port: 3306,
 
   user: "root",
 
-  password: "password",
-  database: "greatbayDB",
+  password: "Welcome1",
+  database: "greatbaydb",
 });
+connection.query = util.promisify(connection.query);
 
-connection.connect((err) => {
-  if (err) throw err;
-  console.log(`connected as id ${connection.threadId}\n`);
-  startChoices();
-});
+
 
 function startChoices() {
   inquirer
     .prompt([
       {
         type: "list",
-        name: "postORbid",
-        message: "would you like to post or bid",
-        choices: ["POST", "BID", "View Items", "Exit"],
+        name: "postOrBid",
+        message: "Would you like to post or bid",
+        choices: ["POST", "BID", "Exit"],
       },
     ])
-    .then(function () {
-      switch (data) {
-        case "POST":
-          post();
-          break;
-        case "BID":
-          bid();
-          break;
-        case "View Items":
-          viewItems();
-          break;
-        case "Exit":
-          exit();
-          break;
+    .then((answer) =>{
+      if(answer.postOrBid ==='POST'){post();}
+      else if(answer.postOrBid){
+        bidAuction();
       }
+      else {
+        console.log("Goodbye!!!");
+      }
+      
     });
 }
 
-function viewItems() {}
+
+var post = async() => {
+  inquirer.prompt([
+    {
+      name: "item",
+      type: "input",
+      message: "Please enter an item for post: ",
+    },
+    {
+      name: "price",
+      type: "input",
+      message: "How much do you want the price?",
+    },
+  ]).then(function (input) { 
+   connection.query(
+    'INSERT into bid SET ?',  
+     [ {
+        item: input.item,
+      },
+      {
+        price: input.price,
+      },
+     ],
+    function (err){
+      if (err) throw err
+      console.log('Updating all price...\n');
+      console.table(input);
+      startChoices();
+    }
+  )
+})
+}
+var item = [];
+var viewItems = async () => {
+  console.log('Selecting all items...');
+  const res = await connection.query('SELECT * FROM bid');
+    for( var i = 0; i < res.length; i++) {
+      item.push({value: res[i].id, name: res[i].item +" "+ res[i].price });
+      console.log(item);
+    }
+  console.log(item);
+  return item;
+};
+var  bidAuction = async() => {
+  inquirer.prompt([
+    {
+      name: "item",
+      type: "choices",
+      message: "Please pick an item to choice from: ",
+      choices: await viewItems(),
+    },
+    {
+      name: "price",
+      type: "input",
+      message: "How much do you want the price?",
+    },
+  ]).then(function (input) { 
+   connection.query(
+    'INSERT into bid SET ?',  
+     [ {
+        item: input.item,
+      },
+      {
+        price: input.price,
+      },
+     ],
+    function (err){
+      if (err) throw err
+      console.log('Updating all price...\n');
+      console.table(input);
+      startChoices();
+    }
+  )
+})
+}
+connection.connect((err) => {
+  if (err) throw err;
+  console.log(`connected as id ${connection.threadId}\n`);
+  startChoices();
+});
--- seed.sql
+++ seed.sql
@@ -1,14 +1,25 @@
 Create database greatbaydb;
 
 drop database greatbaydb;
+
+use greatbaydb;
+
+Create database greatbaydb;
+
+drop database greatbaydb;
 use greatbaydb;
 create table bid (
     id int not null Primary key auto_increment,
     item VARCHAR(30),
+    category VARCHAR(45) NOT NULL,
+    starting_bid INT default 0,
+    highest_bid INT default 0,
     price int 
 );
 
-insert into bid (item, price) values
+insert into bid (item, category, price) values
 ("chairs", 2),
 ("tables", 2),
 ("lamps", 2);
+
+
